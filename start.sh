4010e2b37ed0c5dc11e1f075ab6708a2ba89c13b
MovieFirebots <kesavansivakumar11@gmail.com>
2025-04-24 18:19:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /MsFilmFactoryV2
 fi
-cd /LazyPrincessV2
+cd /MsFilmFactoryV2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
