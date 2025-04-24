bcdecfcd7c4260343bc955f92c89167f0033efe6
MovieFirebots <kesavansivakumar11@gmail.com>
2025-04-24 17:48:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,15 @@
-if [ -z $UPSTREAM_REPO ]
-then
+#!/bin/bash
+
+if [ -z "$UPSTREAM_REPO" ]; then
   echo "Cloning main Repository"
   git clone https://github.com/mrmalikoffl/MsFilmFactoryV2
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MsFilmFactoryV2
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone "$UPSTREAM_REPO" /MsFilmFactoryV2
 fi
-cd /LazyPrincessV2
+
+cd /LazyPrincessV2 || exit 1
 pip3 install -U -r requirements.txt
+
 echo "Starting Bot...."
 python3 bot.py
